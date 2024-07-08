52adc906e69c900c38c98f34b1b42a106256873a
Francescosommellaa <franc.sommella@gmail.com>
2024-07-08 18:49:59 +0200

    add: scss in app.vue

--- src/App.vue
+++ src/App.vue
@@ -22,4 +22,7 @@ export default {
 </template>
 
 <style lang="scss">
+  @use './styles/partials/mixins' as *;
+  @use './styles/partials/variables' as *;
+  @use './styles/general.scss' as *;
 </style>
