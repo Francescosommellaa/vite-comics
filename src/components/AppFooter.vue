a9c56131b8da5fef00726bae0d9d4cddfadbdede
Francescosommellaa <franc.sommella@gmail.com>
2024-07-10 15:45:33 +0200

    add: img in public and create footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,26 +5,51 @@ export default {
 
     data() {
         return {
-            topFooteritems: [
+            topFooterItems: [
                 {
-                    img: "../assets/img/buy-comics-digital-comics.png",
+                    img: "../public/img/buy-comics-digital-comics.png",
                     title: 'DIGITAL COMICS'
                 },
                 {
-                    img: '../assets/img/buy-comics-merchandise.png',
-                    title: 'DIGITAL COMICS'
+                    img: '../public/img/buy-comics-merchandise.png',
+                    title: 'DC MERCHANDISE'
                 },
                 {
-                    img: '../assets/img/buy-comics-subscriptions.png',
-                    title: 'DIGITAL COMICS'
+                    img: '../public/img/buy-comics-subscriptions.png',
+                    title: 'SUBSCRIPTION'
                 },
                 {
-                    img: '../assets/img/buy-comics-shop-locator.png',
-                    title: 'DIGITAL COMICS'
+                    img: '../public/img/buy-comics-shop-locator.png',
+                    title: 'COMIC SHOP LOCATOR'
                 },
                 {
-                    img: '../assets/img/buy-dc-power-visa.svg',
-                    title: 'DIGITAL COMICS'
+                    img: '../public/img/buy-dc-power-visa.svg',
+                    title: 'DC POWER VISA'
+                },
+            ],
+
+            DcComicsItems: [
+                {
+                    link: 'characters'
+                    
+                },
+                {
+                    link: 'characters'
+                },
+                {
+                    link: 'characters'
+                },
+                {
+                    link: 'characters'
+                },
+                {
+                    link: 'characters'
+                },
+                {
+                    link: 'characters'
+                },
+                {
+                    link: 'characters'
                 },
             ]
         }
@@ -37,11 +62,51 @@ export default {
 <template>
     <div class="top-nav">
         <ul class="">
-            <li v-for="(item, index) in topFooteritems">
+            <li v-for="(item, index) in topFooterItems">
                 <img :src="(item.img)" alt="Immagine" />
                 <h5>{{ item.title }}</h5>
             </li>
         </ul>
+    </div>
+
+    <div class="footer">
+        <div class="link-utili">
+            <div>
+                <h3>DC COMICS</h3>
+                <ul v-for="(item, index) in DcComicsItems">
+                    <li>
+                        <a href="#">{{ item.link }}</a>
+                    </li>
+                </ul>
+                <h3>SHOP</h3>
+                <ul v-for="(item, index) in DcComicsItems">
+                    <li>
+                        <a href="#">{{ item.link }}</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <h3>DC</h3>
+                <ul v-for="(item, index) in DcComicsItems">
+                    <li>
+                        <a href="#">{{ item.link }}</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <h3>DC</h3>
+                <ul v-for="(item, index) in DcComicsItems">
+                    <li>
+                        <a href="#">{{ item.link }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <img src="../assets/img/dc-logo-bg.png" alt="">
+        </div>
     </div>
 </template>
 
@@ -56,18 +121,43 @@ export default {
     color: rgba(255, 255, 255, 0.692);
 
     ul {
-    @include center();
-    gap: 20px;
-
-    li {
         @include center();
+        gap: 20px;
 
-        img {
-            height: 40px;
+        li {
+            @include center();
+            gap: 0.4em;
+
+            img {
+            height: 36px;
+            max-width: 36px;
+            }
         }
     }
+
 }
 
+.footer {
+    max-height: 400px;
+    background-image: url(../assets/img/footer-bg.jpg);
+    width: 100%;
+    color: white;
+    @include center(orizzontal);
+    justify-content: space-around;
+    overflow: hidden;
+
+    .link-utili {
+        @include center(orizzontal);
+
+        li {
+            list-style: none;
+            
+            a {
+                color: white;
+                text-decoration: none
+            }
+        }
+    };
 }
 
 </style>
