8eba7b89f118a6db97cdc4682b7567785fb0c350
Francescosommellaa <franc.sommella@gmail.com>
2024-07-10 14:33:34 +0200

    add: top footer a AppFooter e agginte array con img e titolo e ciclo for per stampare in DOM

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,13 +2,72 @@
 
 export default {
     name: "AppFooter",
+
+    data() {
+        return {
+            topFooteritems: [
+                {
+                    img: "../assets/img/buy-comics-digital-comics.png",
+                    title: 'DIGITAL COMICS'
+                },
+                {
+                    img: '../assets/img/buy-comics-merchandise.png',
+                    title: 'DIGITAL COMICS'
+                },
+                {
+                    img: '../assets/img/buy-comics-subscriptions.png',
+                    title: 'DIGITAL COMICS'
+                },
+                {
+                    img: '../assets/img/buy-comics-shop-locator.png',
+                    title: 'DIGITAL COMICS'
+                },
+                {
+                    img: '../assets/img/buy-dc-power-visa.svg',
+                    title: 'DIGITAL COMICS'
+                },
+            ]
+        }
+
+    }
 }
 
 </script>
 
 <template>
+    <div class="top-nav">
+        <ul class="">
+            <li v-for="(item, index) in topFooteritems">
+                <img :src="(item.img)" alt="Immagine" />
+                <h4>{{ item.title }}</h4>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/mixins' as * ;
+@use '../styles/partials/variables' as * ;
+
+.top-nav{
+    @include center(vertical);
+    height: 100px;
+    background-color: #0582F9;
+    color: white;
+
+    ul {
+    @include center();
+    gap: 20px;
+
+    li {
+        @include center();
+
+        img {
+            height: 40px;
+        }
+    }
+}
+
+}
 
 </style>
